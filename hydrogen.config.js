4089717e513abb2829736ab96143bd7b01a93f35
Saikat Santra <56018827+SaikatSantra@users.noreply.github.com>
2022-06-06 13:32:49 +0530

    Shopify Hydrogen app

---
+++ hydrogen.config.js
@@ -0,0 +1,21 @@
+import {defineConfig} from '@shopify/hydrogen/config';
+import {
+  CookieSessionStorage,
+  PerformanceMetricsServerAnalyticsConnector,
+} from '@shopify/hydrogen';
+
+export default defineConfig({
+  shopify: {
+    storeDomain: 'saikatshop.myshopify.com',
+    storefrontToken: 'ee27acc6d1bf49b2bfda78bf41bf0b8a',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+  serverAnalyticsConnectors: [PerformanceMetricsServerAnalyticsConnector],
+});
